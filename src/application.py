03beccf92b77bcd61837b41fec947ceb47ea63e7
HelloImRosie <rosie.liurx@gmail.com>
2022-10-11 21:31:32 -0700

    add apis

--- src/application.py
+++ src/application.py
@@ -22,18 +22,47 @@ def get_health():
 
     return result
 
+@app.route("/students_upload/uni=<uni>&email=<email>&password=<password>&last_name=<last_name>&first_name=<first_name>&middle_name=<middle_name>",
+           methods=["POST", "GET"])
+def insert_student(uni, email, password, last_name, first_name, middle_name):
+    result = StudentsResource.insert_student(uni, email, password, last_name, first_name, middle_name)
+    if result:
+        rsp = Response("STUDENT CREATED", status=200, content_type="text/plain")
+    else:
+        rsp = Response("CREATION FAILED", status=404, content_type="text/plain")
 
-@app.route("/api/students/<uni>/<email>", methods=["GET"])
-def get_student_by_uni(uni, email):
-
+    return rsp
+@app.route("/students/uni=<uni>", methods=["GET"])
+@app.route("/students/email=<email>", methods=["GET"])
+@app.route("/students/uni=<uni>&email=<email>", methods=["GET"])
+@app.route("/students/email=<email>&uni=<uni>", methods=["GET"])
+def get_student_by_uni(uni="", email=""):
     result = StudentsResource.get_by_uni_email(uni, email)
-
 
     if result:
         rsp = Response(json.dumps(result), status=200, content_type="application.json")
     else:
         rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return rsp
 
+
+@app.route("/students_verification/uni=<uni>&email=<email>&token=<token>", methods=["GET"])
+def update_student_status(uni, email, token):
+    is_pending = StudentsResource.student_is_pending(uni, email)
+    if not is_pending:
+        return Response("VERIFICATION FAILED: STUDENT IS NOT PENDING VERIFICATION", status=200,
+                        content_type="text/plain")
+
+    correct_inputs = StudentsResource.verify_student_inputs(uni, email, token)
+    if not correct_inputs:
+        return Response("VERIFICATION FAILED: WRONG TOKEN GIVEN", status=200,
+                        content_type="text/plain")
+
+    verified = StudentsResource.update_student_status(uni, email, token)
+    if verified:
+        rsp = Response("STUDENT VERIFIED", status=200, content_type="text/plain")
+    else:
+        rsp = Response("VERIFICATION FAILED", status=404, content_type="text/plain")
     return rsp
 
 if __name__ == "__main__":
--- src/students_resource.py
+++ src/students_resource.py
@@ -1,5 +1,6 @@
 import pymysql
 import os
+import uuid
 
 
 class StudentsResource:
@@ -23,6 +24,30 @@ class StudentsResource:
         return conn
 
     @staticmethod
+    def insert_student(uni, email, password, last_name, first_name, middle_name):
+        if not (uni and email and last_name and first_name):
+            return False
+        conn = StudentsResource._get_connection()
+        cur = conn.cursor()
+
+        # generate a new token
+        token = uuid.uuid4()
+        if middle_name == "":
+            sql = """
+            INSERT INTO students_login_db.students(uni, last_name, first_name, email, password, status) 
+            values (%s, %s, %s, %s, %s, %s);
+            """
+            cur.execute(sql, args=(uni, last_name, first_name, email, password, token))
+        else:
+            sql = """
+            INSERT INTO students_login_db.students(uni, last_name, first_name, middle_name, email, password, status) 
+            values (%s, %s, %s, %s, %s, %s, %s);
+            """
+            cur.execute(sql, args=(uni, last_name, first_name, middle_name, email, password, token))
+        result = cur.rowcount
+        return True if result == 1 else False
+
+    @staticmethod
     def get_by_uni_email(uni="", email=""):
         # check if uni and email are both empty
         if uni == "" and email == "":
@@ -34,3 +59,38 @@ class StudentsResource:
         result = cur.fetchone()
 
         return result
+
+    @staticmethod
+    def student_is_pending(uni, email):
+        # check if uni and email are correct and student is in pending status
+        if not uni or not email:
+            return False
+        sql = "SELECT status FROM students_login_db.students WHERE uni=%s or email = %s"
+        conn = StudentsResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=(uni, email))
+        result = cur.fetchone()
+        return True if result['status'] != 'Verified' else False
+
+    @staticmethod
+    def verify_student_inputs(uni, email, token):
+        # check if uni and email and token are correct
+        if not uni or not email or not token:
+            return False
+        sql = "SELECT * FROM students_login_db.students WHERE uni=%s or email = %s and status = %s"
+        conn = StudentsResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=(uni, email, token))
+        result = cur.rowcount
+        return True if result == 1 else False
+
+    @staticmethod
+    def update_student_status(uni, email, token):
+        if not uni or not email or not token:
+            return False
+        sql = "UPDATE students_login_db.students SET status = 'Verified' WHERE uni=%s and email=%s and status = %s"
+        conn = StudentsResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=(uni, email, token))
+        result = cur.rowcount
+        return True if result == 1 else False
